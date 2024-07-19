d364edf8c7b5c027b6e84799fba50fda6eae71c7
phh235 <hoangaatrox23504@gmail.com>
2024-07-19 21:46:58 +0700

    change layout

--- tts_app/views.py
+++ tts_app/views.py
@@ -21,10 +21,11 @@ def tts_form(request):
             "region": voice
         })
         headers = {
+            # thinh
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxYzVhYzI3Yy05OGM3LTFhMzktZTA2My02MzE5OWYwYTkyOWQiLCJhdWQiOlsicmVzdHNlcnZpY2UiXSwidXNlcl9uYW1lIjoiaG9hbmdhYXRyb3gyMzUwNEBnbWFpbC5jb20iLCJzY29wZSI6WyJyZWFkIl0sImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0IiwibmFtZSI6ImhvYW5nYWF0cm94MjM1MDRAZ21haWwuY29tIiwidXVpZF9hY2NvdW50IjoiMWM1YWMyN2MtOThjNy0xYTM5LWUwNjMtNjMxOTlmMGE5MjlkIiwiYXV0aG9yaXRpZXMiOlsiVVNFUiJdLCJqdGkiOiJhNzBjNWQyMy03MDBhLTQ3ZTgtOThjMC02ZGZmMTgxMTZiOGUiLCJjbGllbnRfaWQiOiJhZG1pbmFwcCJ9.pmPH7ts8jkWcOu1qZ0g0yMUA0tRUexS3tZHEuSY5LMX8MfVzZeATQD6q6eysvuTQgOr1gte6F2N729pnrcqbHTldCBJsv1cJJKFmkxLD2leGb6bFd4uEoBPpaUAcWr6GhkKFBjLWsBZDTu2d4xAcdiHxaKIMMG2qyWG18fXKEJ_bkyt7gkZL1Wh8i5Ic6bHVWq-VRal6Td128RLNxIhT4V0vLbW7-2V-yxHfa6YS7N9lfWq1Hru5ieD1ogmcsGcMxmdEBI9S3bC0W5oUDtAcRJyGxxAvX_ctHj3TQRpMEnT1G8ol9jhRGV1lDoDW26fGUoGXJbfWX0Rge8UR9SpomQ',
-            'Token-id': '1c5ac405-1919-0ac1-e063-63199f0ac402',
-            'Token-key': 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKj15ikj3q4kNESLkfLrJzGAOFsiScOw3iByTrhj8+c3HAnIGMZ9m+lQFUkfBOuTOdgNQdyA8ReoQniKKWZM63sCAwEAAQ=='
+            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxZDU0YmNkYy1iZjA5LTJmZTctZTA2My02MjE5OWYwYTRiMDciLCJhdWQiOlsicmVzdHNlcnZpY2UiXSwidXNlcl9uYW1lIjoidGhpbmhudnBzMjk0MDhAZnB0LmVkdS52biIsInNjb3BlIjpbInJlYWQiXSwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3QiLCJuYW1lIjoidGhpbmhudnBzMjk0MDhAZnB0LmVkdS52biIsInV1aWRfYWNjb3VudCI6IjFkNTRiY2RjLWJmMDktMmZlNy1lMDYzLTYyMTk5ZjBhNGIwNyIsImF1dGhvcml0aWVzIjpbIlVTRVIiXSwianRpIjoiY2U0ODdmMmItOGJiYy00NTZiLTg2MWYtOTNlOTQ0YjA4OTg3IiwiY2xpZW50X2lkIjoiYWRtaW5hcHAifQ.Y-o49N-bKpwJsYsnwwm4pJQUoqi_wlfzuU2RiysJx9v9wndvSfuxqam1sl1Eu2ldRxCmY16TRtOmaipvnzimwdz7m_JkDg5MDrwN8nYHw30Ts8Ws1s6F2Yhc1b8LCpmd77q-6Dc6NDv6enUErd9EpHMZ45UGFDW1mHZVJQQ0pk9aoAQpvnrVlmTRRbESMFGsnQD16b6RYdJj29921oeH7jfxwm-yJMtsVbieGgTXGjk9EVWY5cO2H01b5c8ZYn9O306YhuOlvB1XM19-xsgFCrYa3C3iJ4Mz6TLJp6HY0DgVoQgJOjqzV260vMoEIE7xWMeWfO0obFARZHFpomnLyw',
+            'Token-id': '1d54c078-af63-5cbb-e063-63199f0a8697',
+            'Token-key': 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJmHrBW152nV+etQ0/o/fWeUKCu5ZyMCn/4ZIob+7X4REyVoMgu/d2DdBFEzTb5O1h7pr4oTlnlG5wQPMuidszsCAwEAAQ=='
         }
 
         response = requests.post(url, headers=headers, data=payload)
